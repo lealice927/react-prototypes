5cceedb010c613add2c9b9a632c191c2f81f0cb4
Alice Le <lealice@gmail.com>
2019-03-26 18:16:58 -0700

    completed first react prototype

--- src/index.js
+++ src/index.js
@@ -1,7 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+
+
+const luckyNumber = () => {
+    return (Math.floor(Math.random() * 1000) + 1);
+};
+
+
+const user = {
+    name: 'Alice',
+    luckyNumber: luckyNumber()
+};
+
+const greeting = (user) => {
+    return (
+        <div className="container">
+            <h1>Welcome {user.name} !!!!!!!!</h1>
+            <h2 className="text-muted"> Your lucky numbers is: {user.luckyNumber}</h2>
+        </div>
+    )
+}
+
 ReactDOM.render(
-    <h1 className="container">React Prototypes!</h1>,
+    greeting(user),
     document.getElementById('root')
 );
